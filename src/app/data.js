a8a17ba89b55fc4406a3fbb73b3bafebd1e0ef4f
sibergold <kaynakca21@hotmail.com>
2024-11-20 02:10:36 +0300

    Last Commits

---
+++ src/app/data.js
@@ -0,0 +1,123 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "TraveLMap",
+    description: "Travel app for location marking",
+    date: "2024-10-28",
+    demoLink: "https://github.com/sibergold/MERNSTACK-TRAVELMAP",
+  },
+  {
+    id: 2,
+    name: "RecipeApp",
+    description: "Recipe app for customization",
+    date: "2024-10-22",
+    demoLink: "https://github.com/sibergold/MERNSTACK-RECIPE-APP",
+  },
+  {
+    id: 3,
+    name: "BudgetPlanner",
+    description: "Plan and track expenses",
+    date: "2024-05-03",
+    demoLink: "https://github.com/sibergold/BudgetPlanner",
+  },
+  {
+    id: 4,
+    name: "E-Commerce",
+    description: "E-commerce site for shopping",
+    date: "2024-10-19",
+    demoLink: "https://github.com/sibergold/MERNSTACK-E-COMMERCE",
+  },
+  {
+    id: 5,
+    name: "ToDoApp",
+    description: "Note-taking app for reminders",
+    date: "2024-10-19",
+    demoLink: "https://github.com/sibergold/MERNSTACK-TODO-APP",
+  },
+  {
+    id: 6,
+    name: "SmartEdu",
+    description: "Course enrollment app",
+    date: "2024-10-13",
+    demoLink: "https://github.com/sibergold/SmartEdu",
+  },
+  {
+    id: 7,
+    name: "BlogProject",
+    description: "Fullstack post app",
+    date: "2024-10-12",
+    demoLink: "https://github.com/sibergold/MERNSTACKBLOGPROJECT",
+  },
+  {
+    id: 8,
+    name: "MovieApp",
+    description: "Movie management app",
+    date: "2024-10-12",
+    demoLink: "https://github.com/sibergold/MOVIE-APP",
+  },
+  {
+    id: 9,
+    name: "ProductCardApp",
+    description: "Card app for products",
+    date: "2024-10-12",
+    demoLink: "https://github.com/sibergold/PRODUCT-CARD-APP",
+  },
+  {
+    id: 10,
+    name: "e-Appointment",
+    description: "e-Appointment backend app for hospitals",
+    date: "2024-06-07",
+    demoLink: "https://github.com/sibergold/eAppointmentServer",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/sibergold",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/omermeric",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Instagram",
+    link: "https://www.instagram.com/krateoss21",
+    icon: "instagram",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/myCV.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
